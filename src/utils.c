179a59a3c61bdffb4ce61b020b5fd76e63d3f507
Ishimoto Shinobu <nagakamira@gmail.com>
2020-08-08 19:56:55 +0300

    Update utils.c

--- src/utils.c
+++ src/utils.c
@@ -7,7 +7,6 @@ void umsg(unsigned char state, const char *msg, ...) {
 		case '0': status = "[ OK ]"; break;
 		case '1': status = "[WARN]"; break;
 		case '2': status = "[FAIL]"; break;
-		default:  status = "[ OK ]"; break;
 	}
 
 	printf("%s %s\n", status, msg);
